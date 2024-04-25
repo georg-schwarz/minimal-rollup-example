8f8c35d110d2ddfd33aebce4168a08dd5f26b6ff
Georg Schwarz <schwarz@group.riehle.org>
2024-04-25 08:42:35 +0200

    Add typescript and import URI for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig as defineViteConfig } from "vite";
+
+import dts from "vite-plugin-dts";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default defineViteConfig({
   root: "",
   cacheDir: "node_modules/.vite/app",
 
-  plugins: [],
+  plugins: [
+    dts({
+      entryRoot: "src",
+      tsconfigPath: "tsconfig.json",
+    }),
+  ],
 
   build: {
     outDir: "dist/",
@@ -13,17 +21,14 @@ export default defineViteConfig({
     reportCompressedSize: true,
 
     commonjsOptions: {
-      // transformMixedEsModules: true,
-      // defaultIsModuleExports: false,
-      // include: [/.js$/],
     },
     lib: {
-      entry: "src/app.js",
+      entry: "src/app.ts",
       fileName: "index",
       formats: ["es"],
     },
     rollupOptions: {
-      plugins: [commonjs()],
+      plugins: [nodeResolve(), commonjs()],
     },
   },
 });
