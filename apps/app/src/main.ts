7d76e6c67ea7e84d13a6ff902bf2de7bfd8826fe
Georg Schwarz <schwarz@group.riehle.org>
2024-04-26 16:17:35 +0200

    Investigate importing callable default exports

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,12 +1,19 @@
 import { URI } from 'vscode-uri';
 import { Either, left, isLeft } from 'fp-ts/lib/Either';
+import { default as JSZip } from 'jszip';
 
 console.log('Start testing some scenarios...');
 
-const isUri = URI.isUri({}); // use functionality of commonjs module
+const isUri = URI.isUri({});
 console.log('Using commonjs libs: ' + !isUri);
+// Learning: default config works fine
 
 const either: Either<true, false> = left(true);
 console.log(
   'Importing nested modules: ' + (isLeft(either) ? either.left : false)
 );
+// Learning: default config works fine
+
+const jszip = JSZip();
+console.log('Instantiating a default import: ' + jszip.name);
+// Replace `import * as JSZip from 'jszip';` with `import { default as JSZip } from 'jszip';`
