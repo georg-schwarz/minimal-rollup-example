ee9242d41b47a6fb8089503a0f5f2caff19dfa39
Georg Schwarz <schwarz@group.riehle.org>
2024-04-26 16:12:12 +0200

    Add example for nested import

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,4 +1,12 @@
 import { URI } from 'vscode-uri';
+import { Either, left, isLeft } from 'fp-ts/lib/Either';
 
-const isUri = URI.isUri({});
-console.log("Hello world " + isUri);
+console.log('Start testing some scenarios...');
+
+const isUri = URI.isUri({}); // use functionality of commonjs module
+console.log('Using commonjs libs: ' + !isUri);
+
+const either: Either<true, false> = left(true);
+console.log(
+  'Importing nested modules: ' + (isLeft(either) ? either.left : false)
+);
